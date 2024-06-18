0a725e0c719ca9c79a4165b9a188fe8f19dd6e57
Aarne Rissanen <rissanen@geoplex.de>
2024-06-18 22:16:39 +0200

    add linting

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+
+app.use(express.json());
 
 app.get('/', (_request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+  response.send('<h1>Hello World!</h1>');
+});
 
 export default app;
