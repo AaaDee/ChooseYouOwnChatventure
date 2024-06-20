912085e85712e47749597d010a4731b4fd06a4cd
Aarne Rissanen <rissanen@geoplex.de>
2024-06-20 21:24:41 +0200

    add background styling

---
+++ frontend/src/components/App/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Title } from '../Title';
+import { StyledApp } from './style';
+
+export const App = () => {
+  return (
+    <StyledApp>
+      <Title />
+    </StyledApp>
+  );
+};
---
+++ frontend/src/components/App/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledApp = styled.div`
+  display: flex;
+  background-color: #486e32;
+  min-height: 100vh;
+`;
