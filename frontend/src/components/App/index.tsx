2ef8cd427482c9d1ddf63487cda8ca3b728b45d1
Aarne Rissanen <rissanen@geoplex.de>
2024-06-25 10:38:14 +0200

    remove commented code

--- frontend/src/components/App/index.tsx
+++ frontend/src/components/App/index.tsx
@@ -5,21 +5,6 @@ import { TextBox } from '../TextBox';
 import { useSelector } from 'react-redux';
 import { selectChoices } from '../../features/choices/selectors';
 
-// const choices: Choice[] = [
-//   {
-//     id: '1',
-//     content: 'fight'
-//   },
-//   {
-//     id: '2',
-//     content: 'run'
-//   },
-//   {
-//     id: '3',
-//     content: 'hide'
-//   }
-// ];
-
 export const App = () => {
   const choices = useSelector(selectChoices);
   console.log('choices', choices);
