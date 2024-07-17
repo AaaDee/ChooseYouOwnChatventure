0b97ba998ca5c4e1b3d896c86c25caf961ca7f8b
Aarne Rissanen <rissanen@geoplex.de>
2024-07-17 23:00:31 +0200

    remove unnecessary test

--- backend/app.test.ts
+++ backend/app.test.ts
@@ -10,10 +10,6 @@ jest.mock('./openai/requestStartPrompt');
 const api = supertest(app);
 
 describe('app works at basic level;', () => {
-  test('hello world is returned', async () => {
-    await api.get('/').expect(200).expect('<h1>Hello World!</h1>');
-  });
-
   test('ping is returned', async () => {
     await api.get('/ping').expect(200).expect('ping!');
   });
