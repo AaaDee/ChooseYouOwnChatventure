c501538c92624ece81ca280711c0e82b7493e5ef
Aarne Rissanen <rissanen@geoplex.de>
2024-07-21 20:46:06 +0200

    add authentication to user logins

--- frontend/src/requests/postRequest.ts
+++ frontend/src/requests/postRequest.ts
@@ -3,6 +3,11 @@ import axios from 'axios';
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 export async function postRequest(endpoint: string, data: object) {
-  const response = await axios.post(`${backendUrl}/${endpoint}`, data);
+  // todo check for existing
+  const token = window.localStorage.getItem('token') || '';
+  const config = {
+    headers: { Authorization: `Bearer ${token}` }
+  };
+  const response = await axios.post(`${backendUrl}/${endpoint}`, data, config);
   return response;
 }
