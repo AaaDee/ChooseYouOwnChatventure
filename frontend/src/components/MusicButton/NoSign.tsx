0c464b28d0410efff27bf21cb7e7870e4ea3c547
Aarne Rissanen <rissanen@geoplex.de>
2024-07-31 22:26:10 +0200

    add no sign as svg

---
+++ frontend/src/components/MusicButton/NoSign.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyledSvg } from './style';
+
+export function NoSign() {
+  return (
+    <StyledSvg
+      width="30.009449"
+      height="30.009449"
+      viewBox="0 0 7.94 7.94"
+      version="1.1"
+      id="svg1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs id="defs1" />
+      <g id="layer1" transform="translate(-0.27715459,-0.2578198)">
+        <circle id="path1" cx="4.2471547" cy="4.2278199" r="3.8800001" />
+        <path d="m 1.4280193,1.4428553 5.3576433,5.632645" id="path4" />
+      </g>
+    </StyledSvg>
+  );
+}
