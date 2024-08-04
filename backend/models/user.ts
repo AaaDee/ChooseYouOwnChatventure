38660879124bec8f95bece18ae19fd6c896b382c
Aarne Rissanen <rissanen@geoplex.de>
2024-08-04 21:42:23 +0200

    remove unnecessary json override

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -8,16 +8,6 @@ const userSchemaDefinition = {
 
 const userSchema = new mongoose.Schema(userSchemaDefinition);
 
-userSchema.set('toJSON', {
-  transform: (_document, returnedObject) => {
-    // todo validation
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  }
-});
-
 export const User = mongoose.model('User', userSchema);
 
 export type UserSchema = InferRawDocType<typeof userSchemaDefinition>;
