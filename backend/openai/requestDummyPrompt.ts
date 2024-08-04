2318b25e8e5c540cea0c519d590bd547ff2d657f
Aarne Rissanen <rissanen@geoplex.de>
2024-08-04 21:36:06 +0200

    remove unnecessary null assertion

--- backend/openai/requestDummyPrompt.ts
+++ backend/openai/requestDummyPrompt.ts
@@ -4,5 +4,5 @@ import { requestCompletions } from './requestCompletions';
 export const requestDummyPrompt = async (): Promise<string | null> => {
   const messages = [{ role: OpenAIRoles.USER, content: 'Say this is a test' }];
   const response = await requestCompletions(messages);
-  return response?.content || null;
+  return response.content;
 };
