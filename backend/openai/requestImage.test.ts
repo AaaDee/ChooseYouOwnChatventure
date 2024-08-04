26b9da30581af09cdacbe0a6c396f6e937f39651
Aarne Rissanen <rissanen@geoplex.de>
2024-08-04 13:40:39 +0200

    test failing image request

--- backend/openai/requestImage.test.ts
+++ backend/openai/requestImage.test.ts
@@ -10,13 +10,26 @@ const mockResponse = { data: [{ b64_json: 'test' }] };
 
 describe('Image requests', () => {
   test('Returns the generated image', async () => {
-    vi.mocked(getOpenAIClient).mockReturnValue({
-      images: {
-        generate: vi.fn().mockResolvedValue(mockResponse)
-      }
-    } as unknown as OpenAI); // inform Typescript that the conversion is intentional
+    const mockGenerate = vi.fn().mockResolvedValue(mockResponse);
+    mockOpenai(mockGenerate);
 
     const image = await requestImage('test');
     expect(image).toEqual('test');
   });
+
+  test('Fails when no image is created', async () => {
+    const failingMockGenerate = vi.fn().mockImplementation(() => {
+      throw new Error('error');
+    });
+    mockOpenai(failingMockGenerate);
+    await expect(requestImage('test')).rejects.toThrowError('image');
+  });
 });
+
+function mockOpenai(mock: () => object) {
+  vi.mocked(getOpenAIClient).mockReturnValue({
+    images: {
+      generate: mock
+    }
+  } as unknown as OpenAI); // inform Typescript that the conversion is intentional
+}
