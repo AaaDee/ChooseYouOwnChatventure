bb030ea038b338d1a460fcbad8ed4c5cdb78c2e3
Aarne Rissanen <rissanen@geoplex.de>
2024-08-04 21:43:23 +0200

    remove unnecessary null assertion

--- backend/features/isPasswordCorrect.ts
+++ backend/features/isPasswordCorrect.ts
@@ -5,8 +5,6 @@ export async function isPasswordCorrect(
   user: UserSchema,
   password: string
 ): Promise<boolean> {
-  const passwordCorrect =
-    user === null ? false : await bcrypt.compare(password, user.passwordHash);
-
+  const passwordCorrect = await bcrypt.compare(password, user.passwordHash);
   return passwordCorrect;
 }
