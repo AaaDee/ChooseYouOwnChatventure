2c0eacd32d9a47f929ce2ae773114bea3603c277
Aarne Rissanen <rissanen@geoplex.de>
2024-09-30 18:56:48 +0200

    migrate front to vitest

--- frontend/src/components/Button/Button.test.tsx
+++ frontend/src/components/Button/Button.test.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { test, vi, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { Button } from './Button';
 
 test('renders content', () => {
-  const mockCallBack = jest.fn();
+  const mockCallBack = vi.fn();
   render(<Button onClick={mockCallBack}>hi!</Button>);
   const element = screen.getByText(/hi!/);
   expect(element).toBeInTheDocument();
 });
 
 test('can be clicked', () => {
-  const mockCallBack = jest.fn();
+  const mockCallBack = vi.fn();
   render(<Button onClick={mockCallBack}>hi!</Button>);
   screen.getByText(/hi!/).click();
   expect(mockCallBack).toBeCalled();
