70d4db27265d50c33a3f2f3f863da9f51c137dbd
Aarne Rissanen <aarne.rissanen@freiheit.com>
2026-02-05 21:46:38 +0100

    change image format to urls

--- frontend/src/components/Illustration/Illustration.tsx
+++ frontend/src/components/Illustration/Illustration.tsx
@@ -1,17 +1,17 @@
 import { useSelector } from 'react-redux';
-import { StyledImage, StyledWrapper } from './style';
 import { selectEntryOrUserLoading } from '../../features/entry/selectors';
-import { Spinner } from '../Spinner/Spinner';
 import {
   selectImage,
   selectImageIsLoading
 } from '../../features/image/selectors';
 import { useLoadImage } from '../../hooks/useLoadImage';
+import { Spinner } from '../Spinner/Spinner';
+import { StyledImage, StyledWrapper } from './style';
 
 export function Illustration() {
   useLoadImage();
-  const imageData = useSelector(selectImage);
-  const imageSrc = `data::image/jpg;base64, ${imageData}`;
+  const image = useSelector(selectImage);
+  const imageSrc = formatImageSource(image);
   const entryOrUserIsLoading = useSelector(selectEntryOrUserLoading);
   const imageIsLoading = useSelector(selectImageIsLoading);
 
@@ -22,4 +22,12 @@ export function Illustration() {
       <StyledImage src={imageSrc} />
     </StyledWrapper>
   );
+}
+
+function formatImageSource(image: string): string {
+  if (image.startsWith('http')) {
+    return image;
+  }
+
+  return `data::image/jpg;base64, ${image}`;
 }
