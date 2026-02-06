ca2ee0f177094de3b40794addccf51cca52a3bb8
Aarne Rissanen <aarne.rissanen@freiheit.com>
2026-02-06 17:55:23 +0100

    fix tests

--- frontend/src/requests/postRequest.test.ts
+++ frontend/src/requests/postRequest.test.ts
@@ -1,11 +1,24 @@
-import { expect, test, vi } from 'vitest';
 import axios from 'axios';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
 import { postRequest } from './postRequest';
 
 vi.mock('axios');
 
-test('Post request calls axios.post', async () => {
-  const postMock = vi.spyOn(axios, 'post');
-  await postRequest('test');
-  expect(postMock).toBeCalled();
+describe('Post request', () => {
+  beforeEach(() => {
+    const localStorageMock = (() => {
+      const store: Record<string, string> = {};
+      return {
+        getItem: vi.fn((key) => store[key] || null)
+      };
+    })();
+
+    vi.stubGlobal('localStorage', localStorageMock);
+  });
+
+  test(' calls axios.post', async () => {
+    const postMock = vi.spyOn(axios, 'post');
+    await postRequest('test');
+    expect(postMock).toBeCalled();
+  });
 });
