3286e4047bed101d17ce1417b81b80a640afc238
Aarne Rissanen <aarne.rissanen@freiheit.com>
2026-02-08 18:22:45 +0100

    add e2e backend test

--- e2e/test.spec.ts
+++ e2e/test.spec.ts
@@ -5,4 +5,10 @@ test('shows the title', async ({ page }) => {
   await expect(page.getByText("Choose Your Own Chatventure")).toBeVisible();
 });
 
+test('ping backend', async ({ page }) => {
+  await page.goto('/api/ping');
+  await expect(page.getByText("ping!")).toBeVisible();
+});
+
+
 
